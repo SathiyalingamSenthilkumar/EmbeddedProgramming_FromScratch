0ca3b33dbc67a5b4aa27c09e36792f4fdf8eb7d9
Sathiya Lingam Senthil Kumar <slsathiya20@gmail.com>
2021-03-30 15:32:38 +0200

    Including the standard library. Calling printf() function. Un commented the division operations which were giving linker errors before including the standard library.

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
  */
 
 #include <stdint.h>
-//#include <stdio.h>
+#include <stdio.h>
 
 
 //-----------Micro controller definitions (STM32F07)------------------
@@ -100,14 +100,14 @@ int main(void)
 
 void init_systick(float timeout){
 
-	//float N_singlecount = (1/(float)SYSCLK);
-	//uint32_t Ncount_reload = timeout/N_singlecount;
+	float N_singlecount = (1/(float)SYSCLK);
+	uint32_t Ncount_reload = timeout/N_singlecount;
 	
 	/* The division part requires few other symbols.
 		Inorder to link with nostdlib the division part is commented out
 	*/
 	
-	uint32_t Ncount_reload = 16000000; //Reload value for a delay of 1 second
+	//uint32_t Ncount_reload = 16000000; //Reload value for a delay of 1 second
 
 	//Registers
 	volatile uint32_t* pSYST_CSR = (uint32_t*) SYST_CSR_ADDR;
@@ -127,6 +127,9 @@ void init_systick(float timeout){
 }
 
 void SysTick_Handler(void){
+	// Printing 
+	printf("Hello from SysTick_Handler\n");
+	
 	// Initializing the pointer variable in this function
 	volatile uint32_t* pGPIOD_ODR   = (uint32_t*)(GPIOD_BASE_ADDR + GPIOD_ODR_OFFST);
 
